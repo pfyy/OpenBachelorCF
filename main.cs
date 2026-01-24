47b0d22aa8418a328361ed4fd2526f65f46d9d06
pfyy <112098515+pfyy@users.noreply.github.com>
2026-01-25 07:59:15 +0800

    archive

---
+++ main.cs
@@ -0,0 +1,25 @@
+import Fiddler;
+
+
+class Handlers
+{
+
+    static function OnBeforeRequest(oSession: Session)
+    {
+        if (!oSession.hostname.EndsWith("hypergryph.com"))
+        {
+            return;
+        }
+
+        if (oSession.HTTPMethodIs("CONNECT"))
+        {
+            oSession.oFlags["x-ReplyWithTunnel"] = "placeholder string";
+            return;
+        }
+
+        oSession.oRequest.headers.UriScheme = "http";
+        oSession.host = "127.0.0.1:8443";
+    }
+
+}
+
